91d162fadbe52c3119ea0589d61dc491de245fb4
RHsanto <rakibulsanto789@gmail.com>
2022-03-29 15:13:47 +0600

    add icons

---
+++ src/components/City/City.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import one from '../../png-1024/bara-imambara-119695.png'
+import two from '../../png-1024/charminar-119696.png'
+import three from '../../png-1024/chennai-central-railway-station-119692.png'
+import four from '../../png-1024/gateway-of-india-119691.png'
+import five from '../../png-1024/hawa-mahal-119697.png'
+import six from '../../png-1024/tajmahal-119689.png'
+import seven from '../../png-1024/victoria-memorial-119694.png'
+import eight from '../../png-1024/vidhana-soudha-119693.png'
+import nine from '../../png-1024/jhulta-minar-155249.png'
+const City = () => {
+  return (
+    <div>
+      <button type="button" class="btn border" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+    Hyderabad 
+    </button>
+
+
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel"> Select a city </h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p className='text-start'>POPULAR CITIES</p>
+        <div className="popular-city">
+          <div className="all-city">
+             <div className="city ">
+             <img src={one} alt="" />
+               <div >
+              <p> Delhi NCR</p>
+               </div>
+               
+              
+             </div>
+             <div className="city ">
+             <img src={two} alt="" />
+               <div >
+              <p>Charminar</p>
+               </div>
+               
+              
+             </div>
+             <div className="city ">
+             <img src={three} alt="" />
+               <div >
+              <p> Chennai</p>
+               </div>
+               
+              
+             </div>
+             <div className="city ">
+             <img src={four} alt="" />
+               <div >
+              <p>Hawa Mahal</p>
+               </div>
+               
+              
+             </div>
+             <div className="city ">
+             <img src={five} alt="" />
+               <div >
+              <p> Hawa Mahal</p>
+               </div>
+               
+              
+             </div>
+             <div className="city ">
+             <img src={six} alt="" />
+               <div >
+              <p> Tajmahal</p>
+               </div>
+               
+              
+             </div>
+             <div className="city ">
+             <img src={seven} alt="" />
+               <div >
+              <p> Victoria </p>
+               </div>
+               
+              
+             </div>
+             <div className="city ">
+             <img src={eight} alt="" />
+               <div >
+              <p> vidhana</p>
+               </div>   
+             </div>
+             <div className="city ">
+             <img src={nine} alt="" />
+               <div >
+              <p> jhulta minar</p>
+               </div>   
+             </div>
+             
+             
+          </div>
+        </div>
+        <div className="others">
+          <div className="text-start mt-4">
+          OTHER CITIES
+
+          <div className="others d-flex">
+            <div className="other ">Chandigarh <br />
+            <><small className='text-danger'>New</small></></div>
+            <div className="other ">Indore  <br />
+            <><small className='text-danger'>New</small></></div>
+            <div className="other ">Kochi   <br />
+            <><small className='text-danger'>New</small></></div>
+            
+
+
+
+          </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  );
+};
+
+export default City;
--- src/components/Header/HeaderTwo/HeaderTwo.js
+++ src/components/Header/HeaderTwo/HeaderTwo.js
@@ -2,22 +2,32 @@ import React from 'react';
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { FaRegHeart } from "react-icons/fa";
 import { AiOutlineUser } from "react-icons/ai";
-import { RiBarChartHorizontalLine } from "react-icons/ri";
+import { AiOutlineBars } from "react-icons/ai";
+import City from '../../City/City';
+
+
 const HeaderTwo = () => {
   return (
     <div>
-      <div className="container mt-2 p-2">
+      <div className="container mt-2">
         <div className="row">
           <div className="col-lg-5 d-flex align-items-center">
-            <ul className=' d-flex menu'>
-              <li><RiBarChartHorizontalLine/> Menu</li>
+            <ul className=' d-flex menu align-items-center'>
+              
+              <li ><AiOutlineBars/> Menu</li>
               <li>Home</li> 
               <li>Shop</li> 
-              <li>Blog</li> 
-              <li>Account</li> 
-              <li>Pages</li> 
+              <li >Blog</li> 
+             
+            
+              <div className="select  mx-4">
+        
+                 <City/>
+              </div>
+              
             </ul>
           </div>
+
           <div className="col-lg-2">
             <h1><span className='red'>RED</span><span>PARTS</span></h1>
           </div>
